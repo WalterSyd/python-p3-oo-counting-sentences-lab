5fcc87db67649aa9de509284801ff034778b5502
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-09-05 21:10:44 +0300

    defined MyString class and created is_question instance

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value = ''):
+    self.value = value
+
+  @property
+  def value(self):
+    return self._value
+
+  @value.setter
+  def value(self, value):
+    if not isinstance (value, str):
+      print("The value must be a string.")
+    else:
+      self._value = value
+
+  def is_sentence(self):
+    if self.value.endswith('.'):
+      return True
+    else:
+      return False
